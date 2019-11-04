769b0125b69dd51a02c76a734b712261032a43ae
prashanth-sams <sams.prashanth@gmail.com>
2019-11-04 12:44:38 +0400

    size validation

--- client-api.gemspec
+++ client-api.gemspec
@@ -8,7 +8,7 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Prashanth Sams"]
   spec.email         = ["sams.prashanth@gmail.com"]
 
-  spec.summary       = "HTTP Rest Api client for RSpec test automation framework that binds within itself"
+  spec.summary       = "HTTP REST API client for testing application APIs based on the ruby’s RSpec framework that binds a complete api automation framework setup within itself"
   spec.homepage      = "https://github.com/prashanth-sams/client-api"
   spec.license       = "MIT"
 
---
+++ spec/client/size_validation_spec.rb
@@ -0,0 +1,110 @@
+describe 'Size validation' do
+
+  it "json response size validator" do
+
+    actual =
+        {
+            "posts": [
+                {
+                    "id": 1,
+                    "title": "Post 1"
+                },
+                {
+                    "id": 2,
+                    "title": "Post 2"
+                },
+                {
+                    "id": 3,
+                    "title": "Post 3"
+                }
+            ],
+            "profile": {
+                "name": [
+                    {
+                        "type": "new",
+                        "more": [
+                            "created": [
+                                "really": {
+                                    "finally": true
+                                }
+                            ],
+                            "created2": [
+                                "really": {
+                                    "finally": false
+                                }
+                            ]
+                        ]
+                    }
+                ]
+            }
+        }
+
+    validate(
+      actual,
+      {
+          "key": "posts->0",
+          "operator": "==",
+          "size": 2,
+          "type": 'hash'
+      },
+      {
+          "key": "posts->0->id",
+          "operator": "==",
+          "value": 1,
+          "size": 0
+      },
+      {
+          "key": "posts->0->id",
+          "operator": "not contains",
+          "size": 1
+      },
+      {
+          "key": "posts->0->id",
+          "operator": "contains",
+          "size": 0
+      },
+      {
+          "key": "posts->1",
+          "operator": "==",
+          "type": "hash",
+          "size": 2
+      },
+      {
+          "key": "posts->1",
+          "operator": "<=",
+          "type": "hash",
+          "size": 2
+      }
+    )
+
+  end
+
+  it "json response size validator x2", :focus do
+
+    actual =
+        [
+            {
+                "id": 11510,
+                "title": "pair of dice, lost",
+                "clues_count": 5
+            },
+            {
+                "id": 11531,
+                "title": "mixed bag",
+                "clues_count": 5
+            }
+        ]
+
+
+    validate(
+      actual,
+      {
+          "key": "",
+          "operator": "==",
+          "size": 2
+      }
+    )
+
+  end
+
+end
