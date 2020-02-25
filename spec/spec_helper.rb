d4dd0518e28465c7dbb9c8ff4fc0e9b8c7ee42e9
prashanth-sams <sams.prashanth@gmail.com>
2020-02-25 19:52:55 +0400

    updated datadog details

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,7 +26,7 @@ RSpec.configure do |config|
     dog = Dogapi::Client.new($api_key, $app_key)
 
     # dog.search_hosts()
-    IO.write('/tmp/msg.txt', dog.search_hosts())
+    IO.write('export.txt', dog.search_hosts())
 
     # dog.service_check('app.is_ok', 'app1', 0, :message => 'Response: 200 OK', :tags => ['env:test'])
     # dog.emit_point('qa.baseline.website.desktop', 10, :host => dog.datadog_host, :device => "automation")
